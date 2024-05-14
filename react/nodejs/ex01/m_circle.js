13bbc64612112da6f6fb402688c316738d3f52b0
LucasKim4321 <165813806+LucasKim4321@users.noreply.github.com>
2024-05-14 13:18:19 +0900

    js21-2

---
+++ react/nodejs/ex01/m_circle.js
@@ -0,0 +1,13 @@
+// export : 내보내기
+export const PI = 3.14159;  // 따로 따로 export
+
+function getArea(radius) {
+    return PI*radius*radius
+}
+function getCircumference(radius) {
+    return 2*PI*radius
+}
+console.log(PI)
+console.log(getArea(3))
+
+export {getArea,getCircumference}  //  한꺼번에 export
---
+++ react/nodejs/index.js
@@ -0,0 +1,3 @@
+// import : 외부 모듈 가져오기
+import {PI, getArea, getCircumference} from './ex01/m_circle.js'
+console.log(PI, getArea(1),getCircumference(1))
