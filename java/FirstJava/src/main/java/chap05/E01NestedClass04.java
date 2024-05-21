8442f5c5957d21bec4decf1d9e54d2732b831811
LucasKim4321 <165813806+LucasKim4321@users.noreply.github.com>
2024-05-21 14:27:29 +0900

    js27-3

--- java/FirstJava/src/main/java/chap05/E01NestedClass04.java
+++ java/FirstJava/src/main/java/chap05/E01NestedClass04.java
@@ -1,9 +1,12 @@
 package chap05;
 
 public class E01NestedClass04 {
-	// 정적 중첩 클래스
-	 E01Student2.Score score = new E01Student2.Score("홍길동");
-	 score.eng = 23;
-	 score.mat = 21;
-	 score.showInfo();
+
+	public static void main(String[] args) {
+		// 정적 중첩 클래스
+		 E01Student2.Score score = new E01Student2.Score("홍길동");
+		 score.eng = 23;
+		 score.mat = 21;
+		 score.showInfo();
+	}
 }
