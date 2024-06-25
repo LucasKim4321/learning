c73f3d9d278ce82e4d24333a6864627e418d5e0a
LucasKim4321 <xowls0131@naver.com>
2024-06-26 00:20:32 +0900

    js51-5

--- react/myproject/src/component/NoteList.js
+++ react/myproject/src/component/NoteList.js
@@ -1,16 +1,24 @@
-const NoteList = ({notes})=> {
+import {useContext} from 'react';
+import {NotesStateContext} from '../pages/NoteMain.js'
+
+import './NoteList.css'
+
+const NoteList = ()=> {
+
+    const notes = useContext(NotesStateContext);
+    console.log(notes)
 
     return (
         <div className="noteList">
             {
-                notes.map((it)=> {
+                notes.map((it)=> (
                     <div className="note">
-                        <div className="id">{it.id}</div>
-                        <div className="title">{it.title}</div>
-                        <div className="content">{it.content}</div>
-                        <div className="date">{it.date}</div>
+                        <div className="id">넘버: {it.id}</div>
+                        <div className="date">날짜: {it.date}</div>
+                        <div className="title">제목: {it.title}</div>
+                        <div className="content">내용: {it.content}</div>
                     </div>
-                })
+                ))
             }
         </div>
     )
