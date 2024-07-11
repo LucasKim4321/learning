4c6dd7e51fc65a86d53740604ff5989dd9779368
LucasKim4321 <165813806+LucasKim4321@users.noreply.github.com>
2024-07-11 13:21:29 +0900

    js63-2

--- spring/springbootWeb-2/src/main/java/com/spring/test/testcontroller/MyRestController.java
+++ spring/springbootWeb-2/src/main/java/com/spring/test/testcontroller/MyRestController.java
@@ -14,12 +14,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.test.testcontroller.vo.MemberVO;
-
-import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/restapi/*")
---
+++ spring/springbootWeb-2/src/main/java/com/spring/test/testcontroller/vo/ArticleVO.java
@@ -0,0 +1,20 @@
+package com.spring.test.testcontroller.vo;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ArticleVO {
+	private int articleNO;
+	private String title;
+	private String content;
+	private String writer;
+		
+}
