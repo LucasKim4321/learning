ec31c6c0a600a3212072b31eed0d7c2db7e725a3
LucasKim4321 <165813806+LucasKim4321@users.noreply.github.com>
2024-07-16 11:40:40 +0900

    js67-1

--- oracle/study/oracle_java5.sql
+++ oracle/study/oracle_java5.sql
@@ -11,3 +11,7 @@ select count(id) from t_member where id='hong10000';
 -- decode( 炼扒侥, 蔼1, true老 版快 贸府, 蔼2, true老 版快 贸府 .... flase老 版快)
  select decode( count(*), 1, 'true', 'false') as isCheck from t_member;
  select decode( count(*), 1, 'true', 'false') as isCheck from t_member where id='hong100';
+ 
+ select * from t_member order by name desc;
+ select * from t_member order by joindate desc;
+ select * from t_member order by id asc;
