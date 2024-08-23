18406884467887c0dbbc0eff6239214588f73978
LucasKim4321 <165813806+LucasKim4321@users.noreply.github.com>
2024-08-23 18:33:00 +0900

    js 94-2

---
+++ mariadb/VisitBusan.sql
@@ -0,0 +1,15 @@
+-- Visit Busan
+
+-- 데이터베이스 생성
+CREATE DATABASE visitbusan;
+
+-- 데이터베이스 선택
+USE visitbusan;
+
+SHOW TABLES;
+
+
+SHOW COLUMNS FROM item;
+SELECT * FROM item;
+DELETE FROM item;
+DROP TABLE item;
